d20c6545bc37be715183caa7a297a8af04ba86b5
vlaner <vlaner@bk.ru>
2020-02-22 00:30:43 +0300

    Добавил функцию ролл (как в дота 2)
    ну ролл в дотке как ролл в дотке знаете если ниче не ввести то от 1 до 100 а если ввел свои числа то рандомные числа твои класс согласен ае сасный

--- botlib/func_vlaner.py
+++ botlib/func_vlaner.py
@@ -4,8 +4,18 @@
 # необходимое каждому модулю команд начало
 from discord.ext import commands
 from bonzoinit import bot
+from random import randint
 
 # функция говорит сама за себя
 @bot.command()
 async def da(ctx): 
     await ctx.send('{0.author.mention}'.format(ctx) + ' ' + "ПИЗДА АХАХАХХАХАХАХАХААХАХААХА")
+
+@bot.command()
+async def roll(ctx, a=None, b=None):
+    if a is None and b is None:
+        await ctx.send('{0.author.mention}'.format(ctx) + ' Random Number is: ' + str(randint(1, 100)))
+    else:
+        a = int(a)
+        b = int(b)
+        await ctx.send('{0.author.mention}'.format(ctx) + ' Random Number is: ' + str(randint(a, b)))
