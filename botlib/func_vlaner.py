3ba249eece5aaeaff3af08437ec34d3258eaff72
vlaner <vlaner@bk.ru>
2020-02-22 22:46:55 +0300

    добавил функцию pict
    генерирует ссылку с сайта https://prnt.sc/

--- botlib/func_vlaner.py
+++ botlib/func_vlaner.py
@@ -4,12 +4,14 @@
 # необходимое каждому модулю команд начало
 from bonzoboot import bot
 from random import randint
+from random import sample
 
 # функция говорит сама за себя
 @bot.command()
 async def da(ctx): 
     await ctx.send('{0.author.mention}'.format(ctx) + ' ' + "ПИЗДА АХАХАХХАХАХАХАХААХАХААХА")
 
+#dota 2 roll
 @bot.command()
 async def roll(ctx, a=None, b=None):
     if a is None and b is None:
@@ -18,3 +20,12 @@ async def roll(ctx, a=None, b=None):
         a = int(a)
         b = int(b)
         await ctx.send('{0.author.mention}'.format(ctx) + ' Random Number is: ' + str(randint(a, b)))
+
+@bot.command()
+async def pict(ctx):
+    symbols = 'abcdefghijklmnopqrstuvwxyz1234567890'
+    url = 'https://prnt.sc/'
+    symbolsStr = ''.join(sample(symbols, 6)) # делаем случайную строку из 6 символов
+    url += symbolsStr # соединяем строку выше с ссылкой url
+    await ctx.send(url)
+
