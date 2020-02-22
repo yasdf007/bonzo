0a4ef03de317f4e2e7335ac9247c901063e13e54
Sannawa <61296585+Sannawa@users.noreply.github.com>
2020-02-22 23:11:15 +0300

    Список команд
    Отправка списка команд. Тут только бог поможет...

--- botlib/func_nohame.py
+++ botlib/func_nohame.py
@@ -4,3 +4,20 @@
 # необходимое каждому модулю команд начало
 from bonzoboot import bot
 
+@bot.command 
+async def jf(ctx): 
+   embed=discord.Embed(
+title="Команды бота ахахахахахахаха",
+colour=0xffff00) 
+#головная часть. Последняя штука для цвета полоски
+embed.add_field(name="b/da", value="ладно", inline=False) 
+#первый шрифт жирный, второй тонкий внизу 
+embed.add_field(name="b/randomcat", value="котик", inline=False) 
+#смещение влево (False) 
+embed.add_field(name="b/ping", inline=False)
+embed.set_author('{0.author.mention}'.format(ctx)) 
+#упоминание отправителей 
+embed.set_footer(text="вставить текст")  
+#маленькая подпись в самом низу
+await send.ctx(embed=embed) 
+#сброс бомбы на пiндосов 
