619fa60859a3c25373cc71dd6a4aae45c4ba66af
yasdf007 <yasdf007@yandex.ru>
2020-02-22 23:29:56 +0300

    функция @sannawa >jf исправлена.
    сместил строчки вправо, добавил отсутствующий value в embed b/ping, комментарии разместил над каждым полем (было вразнобой), удалил set_author (не нашёл решения??)
    
    ещё удалил везде строку чтобы файлы не трогали - это всё наше и мы должны помогать друг другу. (немного философии командной работы :smiley:)
    
    Co-Authored-By: sannawa <sannawa@users.noreply.github.com>

--- botlib/func_nohame.py
+++ botlib/func_nohame.py
@@ -1,23 +1,21 @@
 # created by nohame (c) 2020.
-# вносить дальнейшие изменения в этот файл следует только автору файла.
 
 # необходимое каждому модулю команд начало
 from bonzoboot import bot
+import discord
 
-@bot.command 
-async def jf(ctx): 
+@bot.command()
+async def jf(ctx):
    embed=discord.Embed(
-title="Команды бота ахахахахахахаха",
-colour=0xffff00) 
-#головная часть. Последняя штука для цвета полоски
-embed.add_field(name="b/da", value="ладно", inline=False) 
-#первый шрифт жирный, второй тонкий внизу 
-embed.add_field(name="b/randomcat", value="котик", inline=False) 
-#смещение влево (False) 
-embed.add_field(name="b/ping", inline=False)
-embed.set_author('{0.author.mention}'.format(ctx)) 
-#упоминание отправителей 
-embed.set_footer(text="вставить текст")  
-#маленькая подпись в самом низу
-await send.ctx(embed=embed) 
-#сброс бомбы на пiндосов 
+   title="Команды бота ахахахахахахаха",
+   colour=0xffff00)
+   #головная часть. Последняя штука для цвета полоски
+   embed.add_field(name="b/da", value="ладно", inline=False) 
+   #первый шрифт жирный, второй тонкий внизу 
+   embed.add_field(name="b/randomcat", value="котик", inline=False) 
+   #смещение влево (False) 
+   embed.add_field(name="b/ping", value="пинг!", inline=False)
+   #маленькая подпись в самом низу
+   embed.set_footer(text="вставить текст")
+   #сброс бомбы на пiндосов 
+   await ctx.send(embed=embed)
