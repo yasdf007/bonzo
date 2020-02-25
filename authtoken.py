593996d1b223e028913a40072464c67cf1456506
vlaner <vlaner@bk.ru>
2020-02-26 00:19:08 +0300

    b/help с thumbnail

---
+++ authtoken.py
@@ -0,0 +1,4 @@
+# Created by ムAloneStranger (c) 2020.
+# Этот файл предназначен для хранения токена bonzo и не должен быть опубликован на github, потому он включён в .gitignore
+
+token = 'NjgwMTMyOTA3ODU5NDQzNzkw.XlFztg.9ftpjOoB73PW0yxlTFOYkNmFPLY'
--- botlib/func_nohame.py
+++ botlib/func_nohame.py
@@ -11,7 +11,7 @@ async def help(ctx):
    embed=discord.Embed(
    title='**Команды бота:**', # title - головная часть, colour - hex-код цвета полоски
    colour=0xffff00)
-#   embed.set_thumbnail(url=discord.AppInfo.icon_url) # не работает.
+   embed.set_thumbnail(url='https://cdn.discordapp.com/avatars/680132907859443790/3d059b62a6c5b6dd6fa46fdfd432f009.webp?size=256') # не работает.
    embed.add_field(name='da', value='Отправит в ответ ПИЗДУ)', inline=False) # inline отвечает за смещение вправо (репрезентация в одной строке)
    embed.add_field(name='ping', value='Понг!', inline=False)
    embed.add_field(name='roll', value='Ролит как в доте или между двумя числами', inline=False)
