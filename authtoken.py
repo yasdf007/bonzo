b6ad9fd8cb0b712a064784e22f0a338117ef6f05
ムAloneStranger <23001964+yasdf007@users.noreply.github.com>
2020-02-26 00:23:54 +0300

    удаление пролитого токена
    токен был опубликован вследствие ошибки контрибьютора

--- authtoken.py
+++
@@ -1,4 +0,0 @@
-# Created by ムAloneStranger (c) 2020.
-# Этот файл предназначен для хранения токена bonzo и не должен быть опубликован на github, потому он включён в .gitignore
-
-token = 'NjgwMTMyOTA3ODU5NDQzNzkw.XlFztg.9ftpjOoB73PW0yxlTFOYkNmFPLY'
