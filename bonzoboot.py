97754933815f3be579ddd0f26e240685e3510b55
yasdf007 <23001964+yasdf007@users.noreply.github.com>
2020-04-16 21:39:57 +0300

    комментарии в bonzoboot.py
    закомментировал для понимания тем, кто это возможно будет читать (привет начинающим программистам :+1: )

--- bonzoboot.py
+++ bonzoboot.py
@@ -11,19 +11,20 @@ from time import time
 # для безопасного импорта токена
 import os 
 from dotenv import load_dotenv 
-load_dotenv() 
+load_dotenv() # загружает файл env
 
 game = discord.Game("v0.3.2a rocescrite") # пишем боту в активити
-bot = commands.Bot(command_prefix='bd/', help_command=None)
+bot = commands.Bot(command_prefix='bd/', help_command=None) # лёгкая референс-комманда для нашего бота, задаём префикс и встроенную команду help 
 
 # функция запуска (можно узнать разницу между прочитыванием кода компьютером и связью с discord api)
 def bonzo():
-    global ctimest
+    global ctimest # переменная, содержащая разницу между временем прочтения кода и готовым к работе ботом
     ctimest = time() # таймштамп: код успешно прочитан
     print('/', 'initialization file has been successfully read. starting up bonzo...', sep='\n')
-    bot.run(os.getenv('TOKEN'))
+    bot.run(os.getenv('TOKEN')) # берёт переменную TOKEN из .env
 
 # eval - запуск кода от лица бота овнером через discord.
+# не следует использовать рядовым пользователям. дословно закомментировано не будет (!)
 @bot.command() 
 async def evala(ctx, evcode=None):
     ownerids = [221246477630963722, 196314341572608000, 393807398047055883] # определяем овнеров
@@ -48,7 +49,7 @@ from botlib.func_vlaner import *
 # on_ready выполняется при полной готовности бота к действиям
 @bot.event 
 async def on_ready():
-    global ctimest
+    global ctimest # переменная, содержащая разницу между временем прочтения кода и готовым к работе ботом
     await bot.change_presence(status=discord.Status.online, activity=game) # бот меняет свой статус именно благодаря этой команде (и "играет" в "игру" которую мы задали в строке 13)
     ctimest = time() - ctimest # дельта времени: бот готов к работе
     print('/', 'bonzo has been successfully initialized on ' + platform.platform(), 'timestamp delta is: ' + str(round(ctimest,3)) + 's', 'discord latency is: ' + str(round(bot.latency, 3)) + 's', '/', sep='\n')
