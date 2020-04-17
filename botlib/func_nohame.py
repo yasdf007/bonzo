bd766cfd1563316e8018dd11658c00f8635b2f33
yasdf007 <23001964+yasdf007@users.noreply.github.com>
2020-04-17 14:40:48 +0300

    обновление help, удалены старые [join/leave]vc

--- botlib/func_nohame.py
+++ botlib/func_nohame.py
@@ -17,30 +17,31 @@ async def help(ctx):
    embed.add_field(name='roll', value='Ролит как в доте или между двумя числами', inline=False)
    embed.add_field(name='randomcat', value='Отправляет случайного котика :3', inline=False)
    embed.add_field(name='pict', value='Отправляет случайное изображение из prnt.sc :o', inline=False) 
+   embed.add_field(name='randImg', value='Отправляет случайное изображение из imgur', inline=False)
    embed.add_field(name='serverinfo', value='Показывает информацию о сервере', inline=False)
    embed.add_field(name='clear', value='Очищает последние x сообщений (только для разработчиков)', inline=False)
    embed.add_field(name='evala', value='Исполняет код. (только для создателей бота)', inline=False)
-   embed.add_field(name='joinvc', value='Присоединяется к войсу! Будет молчать как влаdick после месячных!', inline=True)
-   embed.add_field(name='leavevc', value='Покидает войс! Как влаdick после катки в доту!', inline=True)
+   embed.add_field(name='join', value='Присоединяется к войсу! Будет молчать как влаdick после месячных!', inline=True)
+   embed.add_field(name='leave', value='Покидает войс! Как влаdick после катки в доту!', inline=True)
 #  embed.add_field(name='', value='', inline=)
    embed.set_footer(text="/by bonzo/ for @" + ctx.message.author.name) # подпись внизу
    await ctx.send(embed=embed)
 
 # команда присоединения к vc
-@bot.command() 
-async def joinvc(ctx):
-   if ctx.author.voice and ctx.author.voice.channel:
-      channel = ctx.author.voice.channel
-      await channel.connect() #следовать за вождь
-   else:
-      await ctx.send("{0.author.mention}".format(ctx) + " ты че долбоёб зайди в войс")
+# @bot.command() 
+# async def joinvc(ctx):
+#    if ctx.author.voice and ctx.author.voice.channel:
+#       channel = ctx.author.voice.channel
+#       await channel.connect() #следовать за вождь
+#    else:
+#       await ctx.send("{0.author.mention}".format(ctx) + " ты че долбоёб зайди в войс")
 
 # команда отсоединения от vc
-@bot.command() 
-async def leavevc(ctx):
-   voice_client = ctx.bot.voice_clients
-   if voice_client:
-      await ctx.voice_client.disconnect()
-   else:
-      await ctx.send('даун, %s, я не в войсе' %"{0.author.mention}".format(ctx))
+# @bot.command() 
+# async def leavevc(ctx):
+#    voice_client = ctx.bot.voice_clients
+#    if voice_client:
+#       await ctx.voice_client.disconnect()
+#    else:
+#       await ctx.send('даун, %s, я не в войсе' %"{0.author.mention}".format(ctx))
 
