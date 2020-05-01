7a3d7607dea1ec7422d893b8c0ea4ca31b085047
yasdf007 <23001964+yasdf007@users.noreply.github.com>
2020-05-01 22:17:35 +0300

    функция net => сами знаете кого ответ
    прикольна)

--- botlib/func_vlaner.py
+++ botlib/func_vlaner.py
@@ -16,6 +16,10 @@ from io import BytesIO
 async def da(ctx): 
     await ctx.send('{0.author.mention}'.format(ctx) + ' ' + "ПИЗДА АХАХАХХАХАХАХАХААХАХААХА")
 
+@bot.command(pass_context=True)
+async def net(ctx): 
+    await ctx.send('{0.author.mention}'.format(ctx) + ' ' + "ПИДОРА ОТВЕТ АХАХАХАХААХХАХАХА")
+
 #dota 2 roll
 @bot.command()
 async def roll(ctx, a=None, b=None):
