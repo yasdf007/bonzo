320e1a3ce8e8ed1f50a772ade2265c187df549da
ムAloneStranger <23001964+yasdf007@users.noreply.github.com>
2020-11-18 00:02:04 +0300

    hotfix (windows)

--- get-req.bat
+++ get-req.bat
@@ -3,7 +3,7 @@ Title bonzo ExtLib Installer
 echo This Batch script will install all required libraries for running bonzo
 echo /
 pause
-pip install -r requirements.txt
+pip install --user -r requirements.txt
 cls
 echo Everything went ok by far. Enjoy running Bonzo!
 echo /
