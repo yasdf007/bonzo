643a805dc0a74b7fee155635f22ada75fb4d0119
ムAloneStranger <23001964+yasdf007@users.noreply.github.com>
2020-11-19 03:04:43 +0300

    avatar update

--- commands/nohameCog.py
+++ commands/nohameCog.py
@@ -12,7 +12,7 @@ class nohameCog(commands.Cog):
             title='**Команды бота:**',  # title - головная часть, colour - hex-код цвета полоски
             colour=0xffff00)
         embed.set_thumbnail(
-            url='https://cdn.discordapp.com/avatars/680132907859443790/3d059b62a6c5b6dd6fa46fdfd432f009.webp?size=256')
+            url='https://i.ibb.co/Xk7qTy4/BOnzo-1.png')
         # inline отвечает за смещение вправо (репрезентация в одной строке)
         embed.add_field(name='ping', value='Понг!', inline=False)
         embed.add_field(
