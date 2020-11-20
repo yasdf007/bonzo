1194aff38f842eff132c58405cc9ae04b6cf6027
ムAloneStranger <23001964+yasdf007@users.noreply.github.com>
2020-11-20 21:51:14 +0300

    music initial

--- bonzoboot.py
+++ bonzoboot.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 
 load_dotenv()  # загружает файл env
 
-game = discord.Game("b/help | v0.8.12")  # пишем боту в активити
+game = discord.Game("b/help | v0.9.0a")  # пишем боту в активити
 # лёгкая референс-комманда для нашего бота, задаём префикс и встроенную команду help
 bot = commands.Bot(command_prefix=str(os.getenv('PREFIX')), help_command=None)
 
