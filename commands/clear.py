1b6924f7603b1532bd2d97dd29e3a3ab88465cbb
vlaner <vlaner@bk.ru>
2020-11-22 19:48:53 +0300

    обнова clear

--- commands/clear.py
+++ commands/clear.py
@@ -16,21 +16,20 @@ class Clear(commands.Cog):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send('Нужно ввести количество сообщений целым числом')
 
+    async def cog_command_error(self, ctx, error):
+        if isinstance(error, commands.MissingRole):
+            await ctx.send('**слыш,** тебе нельзя такое исполнять')
+
     # функция, удаляющая X сообщений из чата
+    @commands.has_role('bonzodev')
     @commands.command(name='clear', description='Очищает последние x сообщений (только для разработчиков)')
     async def clear(self, ctx, count: int):
-
-        # создание читаемого списка ролей запрашивающего выполнения команды
-        authorroles = [y.name.lower() for y in ctx.author.roles]
-        if 'bonzodev' in authorroles:  # проверяем, есть ли в ролях пользователя роль bonzodev
-            # удаляем запрошенное кол-во сообщений!
-            await discord.TextChannel.purge(ctx.message.channel, limit=count + 1)
-            await ctx.send("очистил %s сообщений!" % count)  # отправляем отчёт
-            await sleep(2)  # ждём 2 секунды
-            # удаляем отчёт
-            await discord.TextChannel.purge(ctx.message.channel, limit=1)
-        else:
-            await ctx.send('{0.author.mention}'.format(ctx) + ' **слыш,** тебе нельзя такое исполнять')
+        # удаляем запрошенное кол-во сообщений!
+        await discord.TextChannel.purge(ctx.message.channel, limit=count + 1)
+        await ctx.send("очистил %s сообщений!" % count)  # отправляем отчёт
+        await sleep(2)  # ждём 2 секунды
+        # удаляем отчёт
+        await discord.TextChannel.purge(ctx.message.channel, limit=1)
 
 
 def setup(bot):
