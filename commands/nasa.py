aaf9a56e7a611894aa69d4abffd7fc63a6ac08de
vlaner <vlaner@bk.ru>
2020-11-23 18:32:01 +0300

    картинка дня от наса

---
+++ commands/nasa.py
@@ -0,0 +1,28 @@
+from discord import Embed
+from discord.ext import commands
+from requests import get
+import json
+name = 'nasapict'
+description = 'Картнка дня от NASA'
+
+
+class Nasa(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name=name, description=description)
+    async def nasapict(self, ctx):
+        embed = Embed(title='Картинка дня от NASA', color=0x0000ff)
+
+        query = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
+        res = get(query)
+        resJson = json.loads(res.text)
+
+        embed.set_image(url=resJson['hdurl'])
+        embed.set_footer(text=resJson['title'])
+
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Nasa(bot))
