99ca4cbacb260571b5983313b943bdf380f0ebf8
ムAloneStranger <23001964+yasdf007@users.noreply.github.com>
2020-12-26 00:02:40 +0300

    qck fix :electric_plug:

--- bonzoboot.py
+++ bonzoboot.py
@@ -3,11 +3,9 @@
 # осуществлять запуск только из этого файла.
 from discord import Intents, Game, Status
 from discord.ext.commands import Bot as bonzoBot, Cog
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from platform import platform
 from time import time
 from os import listdir, getenv
-from database import db
 from dotenv import load_dotenv
 
 load_dotenv()  # загружает файл env
@@ -16,12 +14,10 @@ load_dotenv()  # загружает файл env
 class Bot(bonzoBot):
     def __init__(self):
         intents = Intents.all()
-        self.game = Game("b/help | v1.0 RC1")
-        self.scheduler = AsyncIOScheduler()
+        self.game = Game("b/help | v1.0 RC2")
         self.startTime = None
         self.guild = None
 
-        db.autoSave(self.scheduler)
         super().__init__(command_prefix=getenv('PREFIX'),
                          help_command=None, intents=intents)
 
@@ -37,11 +33,6 @@ class Bot(bonzoBot):
         self.cogsLoad()
         super().run(getenv('TOKEN'))  # берёт переменную TOKEN из .env
 
-    async def update_db(self):
-        db.cursor.executemany(
-            "INSERT into exp (username, UserID) VALUES (%s, %s) ON CONFLICT (UserID) DO NOTHING;", ((member.name, member.id) for member in self.guild.members if not member.bot))
-        await db.commit()
-
     @Cog.listener()
     async def on_ready(self):
 
@@ -49,8 +40,6 @@ class Bot(bonzoBot):
         # бот меняет свой статус именно благодаря этой команде (и "играет" в "игру")
         await self.change_presence(status=Status.online, activity=self.game)
         self.load_extension('commands.music')
-        self.scheduler.start()
-        await self.update_db()
 
         endTime = time() - self.startTime
 
