99e609a410d256e64d29c848a73035298658e5af
vlaner <43291931+vlaner@users.noreply.github.com>
2021-02-16 17:49:54 +0300

    Create dvach.py

---
+++ commands/dvach.py
@@ -0,0 +1,33 @@
+from discord.ext.commands import Cog, CommandOnCooldown, command, cooldown
+import requests
+from random import choice
+import json
+name = '2ch'
+description = 'Рандомное видео с двача'
+
+
+class Dvach(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.USERAGENT = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        }
+        self.URL = 'https://api.randomtube.xyz/video.get'
+
+    async def cog_command_error(self, ctx, error):
+        if isinstance(error, CommandOnCooldown):
+            await ctx.send(error)
+
+    @cooldown(rate=2, per=13)
+    @command(name=name, description=description)
+    async def dvach(self, ctx):
+        res = requests.get(self.URL, headers=self.USERAGENT,
+                           params={'board': 'b'})
+        resJson = json.loads(res.text)
+        link = choice(resJson['response']['items'])['url']
+        await ctx.send(link)
+
+
+def setup(bot):
+    bot.add_cog(Dvach(bot))
