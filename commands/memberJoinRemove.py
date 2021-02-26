38ff1af4013cb11467c3b37148a04c6ff400e398
vlaner <vlaner@bk.ru>
2021-02-26 18:13:12 +0300

    Апдейт евентов добавления/удаления из бд

--- commands/memberJoinRemove.py
+++ commands/memberJoinRemove.py
@@ -10,11 +10,12 @@ class memberJoinRemove(Cog):
     @Cog.listener()
     async def on_member_join(self, member):
         db.execute(
-            "INSERT into exp (UserID) VALUES %s ON CONFLICT (UserID) DO NOTHING;", (member.id,))
+            "INSERT into exp (UserID, serverId) VALUES (%s,%s);", (member.id, member.guild.id))
 
     @Cog.listener()
     async def on_member_remove(self, member):
-        db.execute('DELETE FROM exp WHERE UserID = %s', (member.id,))
+        db.execute('DELETE FROM exp WHERE UserID = %s and serverId=%s',
+                   (member.id, member.guild.id))
 
 
 def setup(bot):
