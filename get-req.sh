dcdae2c12c04ece1cd3e220c436dfeebd08280c3
ムAloneStranger <23001964+yasdf007@users.noreply.github.com>
2021-07-03 05:49:15 +0300

    необходимая библиотека

--- get-req.sh
+++ get-req.sh
@@ -2,5 +2,5 @@
 echo "this script installs all required libraries to run bonzo properly."
 read -p "press return to continue."
 pip3 install --user -r requirements.txt
-clear
+echo "@\n@\n@"
 echo "everything went ok by far. enjoy using bonzo :^)"
