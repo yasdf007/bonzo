311d96c82947573d44ce7faebfdff2af81826d3f
ムAloneStranger <23001964+yasdf007@users.noreply.github.com>
2021-07-08 01:09:53 +0300

    /invite!

--- commands/invite.py
+++ commands/invite.py
@@ -1,5 +1,7 @@
 from discord import Embed
-from discord.ext.commands import Cog, command
+from discord.ext.commands import Cog
+from discord_slash import SlashContext, cog_ext
+from bonzoboot import guilds
 
 name = 'invite'
 description = 'Ссылка на приглашение бота'
@@ -10,16 +12,16 @@ class invite(Cog):
         self.bot = bot
 
     # функция, отправляющая инвайт-ссылку бота в чат
-    @command(name=name, description=description)
-    async def invite(self, ctx):
+    @cog_ext.cog_slash(name=name, description=description, guild_ids=guilds)
+    async def invite(self, ctx: SlashContext):
         embedd = Embed(
             title='**зовём /bonzo/ на ваш сервер...**', colour=0xb84000)
         embedd.set_thumbnail(
             url='https://i.ibb.co/Xk7qTy4/BOnzo-1.png')
         embedd.add_field(
             name='кликните вот', value='[сюда](https://discordapp.com/api/oauth2/authorize?client_id=680132907859443790&permissions=8&scope=bot)')
-        embedd.set_footer(text="/by bonzo/ for @" + ctx.message.author.name)
-        await ctx.message.reply(embed=embedd)
+        embedd.set_footer(text="/by bonzo/ for @" + ctx.author.name)
+        await ctx.send(embed=embedd)
 
 
 def setup(bot):
