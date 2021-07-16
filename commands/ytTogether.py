73cbf0dd5507ca4571dd2c566881c09f4b60faec
vlaner <vlaner@bk.ru>
2021-07-17 00:34:16 +0300

    Совместный просмотр видео на ютубе прямо в дискорде

---
+++ commands/ytTogether.py
@@ -0,0 +1,31 @@
+from discord.ext.commands import Cog, command, bot_has_permissions
+from discord.ext.commands.errors import CommandInvokeError, BotMissingPermissions
+from discordTogether import DiscordTogether
+
+name = 'youtube'
+description = 'Создает канал для совместного просмотра видео'
+
+
+class YtTogether(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.togetherControl = DiscordTogether(bot)
+
+    async def cog_command_error(self, ctx, error):
+        if isinstance(error, BotMissingPermissions):
+            await ctx.send(f'Не могу создать инвайт (нет прав)')
+        if isinstance(error, CommandInvokeError):
+            await ctx.send(f'Найдо зайти в войс')
+
+    @bot_has_permissions(create_instant_invite=True)
+    @command(name=name, description=description)
+    async def start(self, ctx):
+        if not ctx.author.voice.channel.id:
+            raise CommandInvokeError
+
+        link = await self.togetherControl.create_link(ctx.author.voice.channel.id, '755600276941176913')
+        await ctx.send(f'Зайти\n{link}')
+
+
+def setup(bot):
+    bot.add_cog(YtTogether(bot))
