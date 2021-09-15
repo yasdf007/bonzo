1d0bc7d257be52a62b930c36924d6f73f20226b5
ムAloneStranger <23001964+yasdf007@users.noreply.github.com>
2021-09-16 01:14:16 +0300

    Виртуальная среда для оптимизации сборки
    Co-Authored-By: vlaner <43291931+vlaner@users.noreply.github.com>

--- get-req.sh
+++ get-req.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-echo "this script installs all required libraries to run bonzo properly."
-read -p "press return to continue."
-pip3 install --user -r requirements.txt
-clear
+
+echo "this script creates virtual environment for bonzo dependencies."
+echo "Press enter to continue"
+read -s
+echo "Processing..."
+
+python3 -m venv env
+
+. ./env/bin/activate
+
+pip3 install -r requirements.txt
+
+echo $'@\n@\n@'
 echo "everything went ok by far. enjoy using bonzo :^)"
