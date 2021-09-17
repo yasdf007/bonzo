5576a644c652e24b8e003f9d1d8668cfef696912
ムAloneStranger <23001964+yasdf007@users.noreply.github.com>
2021-09-17 17:33:15 +0300

    Предварительный тест наличия пользовательских настроек

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
 from os import getenv
 from dotenv import load_dotenv
+from colorama import Fore, Back, Style
 
 load_dotenv()
 
 guilds = None
-OWNER_IDS = [int(id) for id in getenv('OWNER_IDS').split(',')]
-prefix ="b/"
+try:
+    OWNER_IDS = [int(id) for id in getenv('OWNER_IDS').split(',')]
+except:
+    OWNER_IDS = None
+    print(f'{Fore.GREEN} Config: {Style.RESET_ALL} Owners unspecified.')
+
+try:
+    prefix = getenv('PREFIX')
+except:
+    prefix ="b/"
+    print(f'{Fore.GREEN} Config: {Style.RESET_ALL} Prefix unspecified. Using default prefix {prefix}.')
