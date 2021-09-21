32181b2777e96e0266059f32f2e0013b9b7eb8dc
vlaner <vlaner@bk.ru>
2021-09-21 16:25:41 +0300

    Виселица в префиксах + слеши

--- commands/hangman.py
+++ commands/hangman.py
@@ -1,6 +1,8 @@
+from discord.ext.commands.core import group, guild_only
 from discord_slash.context import SlashContext
 from commands.resources.hangman.Hangman import Hangman
-from discord.ext.commands import Cog
+from discord.ext.commands import Cog, command, group
+from discord.ext.commands.context import Context
 import asyncio
 from discord_slash import SlashContext, cog_ext
 from config import guilds
@@ -12,61 +14,87 @@ class GameHangman(Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @guild_only()
+    @group(name='hangman', description='Начать игру виселица', invoke_without_command=True)
+    async def gameHangman_prefix(self, ctx: Context):
+        await self.gameHangman(ctx)
+
     @cog_ext.cog_subcommand(base='hangman', name='start', description='Начать игру виселица')
+    async def gameHangman_slash(self, ctx: SlashContext):
+        await self.gameHangman(ctx)
+
     async def gameHangman(self, ctx):
         if ctx.author.bot:
             return
 
-        if str(ctx.guild.id) in self.games:
+        if isinstance(ctx, Context):
+            guild_id = ctx.guild.id
+        if isinstance(ctx, SlashContext):
+            guild_id = ctx.guild_id
+
+        if str(guild_id) in self.games:
             await ctx.send('Игра идет')
             return
 
-        self.games[str(ctx.guild.id)] = [
+        self.games[str(guild_id)] = [
             f'{ctx.author.id}',
             {'started': False},
             {'end': False}
         ]
 
-        while str(ctx.guild.id) in self.games and self.games[str(ctx.guild.id)][2]['end'] == False:
+        while str(guild_id) in self.games and self.games[str(guild_id)][2]['end'] == False:
             await ctx.send(f'Игра начнется через 10с, игрок {ctx.author}\n/hangman stop для остановки')
             await asyncio.sleep(10)
 
-            if not (str(ctx.guild.id) in self.games):
+            if not (str(guild_id) in self.games):
                 break
 
-            if self.games[str(ctx.guild.id)][2]['end'] == True:
-                self.games.pop(str(ctx.guild.id))
+            if self.games[str(guild_id)][2]['end'] == True:
+                self.games.pop(str(guild_id))
                 return
 
-            self.games[str(ctx.guild.id)][1] = True
+            self.games[str(guild_id)][1] = True
             hangman = Hangman(ctx, ctx.author.id)
             isAfk = await hangman.play()
 
-            if str(ctx.guild.id) in self.games:
-                self.games[str(ctx.guild.id)][1] = False
+            if str(guild_id) in self.games:
+                self.games[str(guild_id)][1] = False
 
             if isAfk == True:
-                self.games.pop(str(ctx.guild.id))
+                self.games.pop(str(guild_id))
+
+    @guild_only()
+    @gameHangman_prefix.command(name='stop', description='Остановить игру виселица')
+    async def stop_prefix_(self, ctx: Context):
+        await self.stop(ctx)
 
     @cog_ext.cog_subcommand(base='hangman', name='stop', description='Остановить игру виселица')
-    async def stop(self, ctx: SlashContext):
+    async def stop_slash_(self, ctx: SlashContext):
+        await self.stop(ctx)
+
+    async def stop(self, ctx):
         if ctx.author.bot:
             return
 
-        if not str(ctx.guild.id) in self.games:
+        if isinstance(ctx, Context):
+            guild_id = ctx.guild.id
+        if isinstance(ctx, SlashContext):
+            guild_id = ctx.guild_id
+
+        if not str(guild_id) in self.games:
             await ctx.send('Игра не идет')
             return
 
-        if str(ctx.author.id) != self.games[str(ctx.guild.id)][0]:
+        if str(ctx.author.id) != self.games[str(guild_id)][0]:
             await ctx.send('Игру может остановить только тот, кто ее начал')
             return
 
-        if self.games[str(ctx.guild.id)][1] == True:
+        if self.games[str(guild_id)][1] == True:
             await ctx.send('Игра будет остановлена после текущей игры')
-            self.games[str(ctx.guild.id)][2]['end'] = True
+            self.games[str(guild_id)][2]['end'] = True
             return
 
-        self.games[str(ctx.guild.id)][2]['end'] = True
+        self.games[str(guild_id)][2]['end'] = True
         await ctx.send('Игра остановлена')
 
 
