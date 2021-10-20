73b4398d9cbf0f7d00e00f2bab69618d2ae5e6f7
ムAloneStranger <23001964+yasdf007@users.noreply.github.com>
2021-10-20 12:25:30 +0300

    Автоматизированный error теперь сообщает внутренний код ошибки
    Python предупреждает о том, что корутина randCol2 используется без await <-- необходим фикс @vlaner

--- commands/resources/AutomatedMessages.py
+++ commands/resources/AutomatedMessages.py
@@ -1,14 +1,18 @@
 from discord import Embed
-from .animationFW import randCol
+from .animationFW import randCol2
 
 class automata:
 
-    def generateEmbErr(x: str):
+    def generateEmbErr(x: str, error=None):
         result = Embed(
             title=f'**{x}**',
-            color=randCol()
         )
-        result.set_footer(text='Error message / by Bonzo /')
+        randCol2(result)
+        if error:
+            readableErr = type(error).__name__
+            result.set_footer(text=f'Error message / by Bonzo / {readableErr}')
+        else:
+            result.set_footer(text='Error message / by Bonzo /')
         return result
 
     # def generateEmbInfo(x: str):
