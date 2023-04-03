19e72fbe95245efab9120f805c8a999811ad4feb
vlaner <vlaner@bk.ru>
2023-04-03 21:23:48 +0300

    Абстракция картинки с опытом + некоторые улучшения

---
+++ commands/resources/xp_system/image.py
@@ -0,0 +1,88 @@
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+
+from contextlib import asynccontextmanager
+
+class ImageGeneration:
+    template: Image.Image
+    progress_bar: Image.Image
+    font: ImageFont.FreeTypeFont
+
+    def __init__(self):
+        self.template = Image.open("./static/rankTemplate.png")
+        self.progress_bar = Image.open("./static/progressBar.png")
+        self.font = ImageFont.truetype("./static/arial.ttf", 14)
+
+    def createMask(self, photo):
+        with Image.new("L", photo.size, 0) as mask:
+            drawMask = ImageDraw.Draw(mask)
+            drawMask.ellipse((0, 0) + photo.size, fill=255)
+
+            return mask
+
+    def __enter__(self):
+        return self
+
+    @asynccontextmanager
+    async def create_card(self, username: str, xp: int, xp_to_level_up: int, lvl: int, rank: int, photo_bytes: bytes, percents: float, all_ranks: int):
+        fullBlack = (0, 0, 0)
+
+        with Image.open(BytesIO(photo_bytes)) as user_pfp:
+            template = self.template.copy()
+            bar = self.progress_bar.copy()
+
+            draw = ImageDraw.Draw(template)
+
+            mask = self.createMask(user_pfp)
+
+            avatarSize = (100, 100)
+
+            rezised = user_pfp.resize(avatarSize)
+            mask = mask.resize(avatarSize)
+
+            barWidth = bar.size[0]
+            croppedBar = bar.crop((0, 0, barWidth * (percents) / 100, 45))
+
+            template.paste(rezised, (15, 15), mask)
+            template.paste(croppedBar, (100, 255), croppedBar)
+
+            percentsText = f"{percents}%"
+            textWidth = self.font.getsize(percentsText)[0]
+
+            draw.text(
+                ((650 - textWidth) / 2, 270),
+                percentsText,
+                fullBlack,
+                font=self.font,
+                align="right",
+            )
+
+            draw.text(
+                (130, 12), f"{username}", fullBlack, font=self.font, align="center"
+            )
+
+            draw.text(
+                (130, 43),
+                f"RANK:{rank}/{all_ranks}",
+                fullBlack,
+                font=self.font,
+                align="center",
+            )
+
+            draw.text(
+                (130, 73),
+                f"EXP: {xp}/{xp_to_level_up}",
+                fullBlack,
+                font=self.font,
+                align="center",
+            )
+
+            draw.text(
+                (130, 102), f"LVL: {lvl}", fullBlack, font=self.font, align="center"
+            )
+            
+            b = BytesIO()
+            template.save(b, "png")
+            b.seek(0)
+
+            yield b
--- commands/xpSystem.py
+++ commands/xpSystem.py
@@ -13,12 +13,11 @@ from discord.ext.commands import NoPrivateMessage as NoPrivateMsg
 from discord.ext.commands.context import Context
 from discord import Embed, File, Asset
 from datetime import datetime, timedelta
-from PIL import Image, ImageDraw, ImageFont
-from io import BytesIO
 from .resources.AutomatedMessages import automata
 
 from .resources.xp_system.abc import XpStrategy
 from .resources.xp_system.xp import OriginalXP
+from .resources.xp_system.image import ImageGeneration
 
 from dependencies.repository.member_info.abc import MemberHandlerRepository
 
@@ -29,10 +28,11 @@ class NoPrivateMessage(CommandError):
 
 
 class AddXP(Cog):
-    def __init__(self, bot, xp_strategy: XpStrategy):
+    def __init__(self, bot, xp_strategy: XpStrategy, image_creation: ImageGeneration):
         self.bot = bot
         self.xp_strategy = xp_strategy
         self.members_repo: MemberHandlerRepository = self.bot.dependency.members_repo
+        self.image_creation = image_creation
 
     async def cog_command_error(self, ctx, error):
         raise error
@@ -195,86 +195,18 @@ class AddXP(Cog):
     @hybrid_command(name="rank", description="Показывает персональную карточку с уровнем")
     async def rank(self, ctx):
         result = await self.members_repo.rank(ctx.guild.id, ctx.author.id)
-        await (
-            await self.bot.loop.run_in_executor(
-                None, self.asyncRankCard, ctx, result.xp, self.xp_strategy.level_from_xp(result.xp), result.rank, result.overall_ranks
-            )
-        )
+        
+        photo_bytes = await Asset.read(ctx.author.display_avatar.with_format("png"))
+        
+        lvl = self.xp_strategy.level_from_xp(result.xp)
+        
+        percents = self.percentsToLvlUp(result.xp, lvl)
 
-    async def createMask(self, photo):
-        with Image.new("L", photo.size, 0) as mask:
-            drawMask = ImageDraw.Draw(mask)
-            drawMask.ellipse((0, 0) + photo.size, fill=255)
-
-            return mask
-
-    async def asyncRankCard(self, ctx, xp, lvl, rank, maxRank):
-        fullBlack = (0, 0, 0)
-        reqImage = await Asset.read(ctx.author.display_avatar.with_format("png"))
-
-        with Image.open(BytesIO(reqImage)) as userProfilePhoto:
-            with Image.open("./static/rankTemplate.png") as template:
-                with Image.open("./static/progressBar.png") as bar:
-                    draw = ImageDraw.Draw(template)
-                    font = ImageFont.truetype("./static/arial.ttf", 14)
-
-                    mask = await self.createMask(userProfilePhoto)
-
-                    avatarSize = (100, 100)
-
-                    rezised = userProfilePhoto.resize(avatarSize)
-                    mask = mask.resize(avatarSize)
-
-                    percents = self.percentsToLvlUp(xp, lvl)
-                    xpToNextLVL = self.xp_strategy.xp_from_level(lvl+1)
-                    
-                    barWidth = bar.size[0]
-                    croppedBar = bar.crop((0, 0, barWidth * (percents) / 100, 45))
-
-                    template.paste(rezised, (15, 15), mask)
-                    template.paste(croppedBar, (100, 255), croppedBar)
-
-                    percentsText = f"{percents}%"
-                    textWidth = font.getsize(percentsText)[0]
-
-                    draw.text(
-                        ((650 - textWidth) / 2, 270),
-                        percentsText,
-                        fullBlack,
-                        font=font,
-                        align="right",
-                    )
-
-                    draw.text(
-                        (130, 12), f"{ctx.author}", fullBlack, font=font, align="center"
-                    )
-
-                    draw.text(
-                        (130, 43),
-                        f"RANK:{rank}/{maxRank}",
-                        fullBlack,
-                        font=font,
-                        align="center",
-                    )
-
-                    draw.text(
-                        (130, 73),
-                        f"EXP: {xp}/{xpToNextLVL}",
-                        fullBlack,
-                        font=font,
-                        align="center",
-                    )
-
-                    draw.text(
-                        (130, 102), f"LVL: {lvl}", fullBlack, font=font, align="center"
-                    )
-
-                    with BytesIO() as temp:
-                        template.save(temp, "png")
-                        temp.seek(0)
-                        await ctx.send(file=File(fp=temp, filename="now.png"))
+        async with self.image_creation.create_card(ctx.author, result.xp, self.xp_strategy.xp_from_level(lvl+1), lvl, result.rank, photo_bytes, percents, result.overall_ranks) as card:
+            await ctx.send(file=File(fp=card, filename="now.png"))
 
 
 async def setup(bot):
     xp_strategy = OriginalXP()
-    await bot.add_cog(AddXP(bot, xp_strategy))
+    image_gen = ImageGeneration()
+    await bot.add_cog(AddXP(bot, xp_strategy, image_gen))
