391fd7ff749486e102e51693b2a297e96dc54187
vlaner <vlaner@bk.ru>
2023-04-03 20:31:02 +0300

    Абстракция опыта

--- commands/resources/xp_system/xp.py
+++ commands/resources/xp_system/xp.py
@@ -1,14 +1,8 @@
-from .abc import XpStrategy, XpValues
+from .abc import XpStrategy
 
-class IntegerXpValues(XpValues):
+class OriginalXP(XpStrategy):
     message_xp = 1
     voice_xp = 10
-
-class PowerStrategy(XpStrategy):
-    xp_values = None
-
-    def __init__(self, xp_values: XpValues):
-        self.xp_values = xp_values
 
     def level_from_xp(self, xp: int):
         return int((xp / 60) ** 0.5)
