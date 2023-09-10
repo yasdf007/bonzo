feb5b952d3657413076dc4f99159c6c703c91db1
ムAloneStranger <23001964+yasdf007@users.noreply.github.com>
2023-09-10 19:08:21 +0300

    upd windows deployment script

--- get-req.bat
+++ get-req.bat
@@ -3,6 +3,8 @@ Title bonzo ExtLib Installer
 echo This Batch script will install all required libraries for running bonzo
 echo /
 pause
+python3 -m venv env
+. \env\Scripts\Activate
 pip install --user -r requirements.txt
 cls
 echo Everything went ok by far. Enjoy running Bonzo!
