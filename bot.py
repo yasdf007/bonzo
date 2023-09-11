3f508bdc4523f421d0ecd1e912f74e5befdac07f
ムAloneStranger <23001964+yasdf007@users.noreply.github.com>
2023-09-11 23:50:27 +0300

    formatting main file

--- bot.py
+++ bot.py
@@ -1,28 +1,24 @@
 # IMPORT START
 
-print("loading...")
-print("-----------------------------")
+import logging
+from os import getenv
+from pathlib import Path
+from platform import platform
+from time import time
 
 import discord
-
-from discord              import Intents, Game, Status, Message
-from discord.ext.commands import Cog, when_mentioned_or, Bot as bonzoBot
-
-from config                               import OWNER_IDS, PREFIX
-
-from colorama import Fore, Style
-from dotenv   import load_dotenv
-from time     import time
-
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from colorama import Fore, Style
+from discord import Game, Intents, Message, Status
+from discord.ext.commands import Bot as bonzoBot
+from discord.ext.commands import Cog, when_mentioned_or
+from dotenv import load_dotenv
 
-from platform import platform
-from os       import getenv
-from pathlib import Path
-
+from config import OWNER_IDS, PREFIX
 from dependencies.repository.prefix.abc import PrefixRepository
 
-import logging
+print("loading...")
+print("-----------------------------")
 
 
 print(Fore.GREEN + "libs imported")
@@ -33,24 +29,27 @@ print("-----------------------------" + Fore.MAGENTA)
 # IMPORT END
 
 
-
 load_dotenv()  # загружает файл env
 
 logger = logging.getLogger("discord")
 logger.setLevel(logging.DEBUG)
 
 
-handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="a")
+handler = logging.FileHandler(
+    filename="discord.log", encoding="utf-8", mode="a")
 handler.setFormatter(
     logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
 )
 logger.addHandler(handler)
 
+
 class Bot(bonzoBot):
     def __init__(self, prefix_repo: PrefixRepository):
         intents = Intents.default()
+        intents.presences = True
         intents.members = True
-        intents.message_content = True
+        # it is mandatory to request access to this intent explicitly via discord dev portal
+        intents.message_content = False
 
         self.prefix_repo = prefix_repo
 
@@ -61,17 +60,18 @@ class Bot(bonzoBot):
             owner_ids=OWNER_IDS,
         )
 
-        self.game = Game(f"@Bonzo init | stacknox2")
+        self.game = Game("@Bonzo init | stacknox2")
         self.scheduler = AsyncIOScheduler()
         self.startTime = None
-    
+
     async def setup_hook(self):
         await self.cogsLoad()
 
         # await self.tree.sync()
 
     async def cogsLoad(self):
-        cmds = [x.stem for x in Path('./commands').iterdir() if x.suffix == '.py' and x.is_file()]
+        cmds = [x.stem for x in Path(
+            './commands').iterdir() if x.suffix == '.py' and x.is_file()]
         total = len(cmds)
 
         for curr, cmd in enumerate(cmds, start=1):
@@ -79,17 +79,16 @@ class Bot(bonzoBot):
                 await self.load_extension(f"commands.{cmd}")
                 print(f"cog {cmd} load, {curr}/{total}")
 
-            except Exception as error: # something in cog wrong
+            except Exception as error:  # something in cog wrong
                 print(f"error in cog {cmd}, {curr}/{total} | {error}")
                 logging.error(f"cog filename not load: {error}")
-
 
     async def _get_prefix(self, bot, message: Message):
         if not message.guild:
             return when_mentioned_or(PREFIX)(bot, message)
-        
+
         guild_prefix = await self.prefix_repo.prefix_for_guild(guild_id=message.guild.id) or PREFIX
-        
+
         return when_mentioned_or(guild_prefix)(bot, message)
 
     def run(self):
@@ -115,15 +114,15 @@ class Bot(bonzoBot):
 
     @Cog.listener()
     async def on_resumed(self):
-        print(Fore.GREEN +  "-----------------------\nbot resumed\n-----------------------" + Style.RESET_ALL)
+        print(Fore.GREEN + "-----------------------\nbot resumed\n-----------------------" + Style.RESET_ALL)
         logging.info("bot resumed")
 
     @Cog.listener()
     async def on_disconnect(self):
-        print(Fore.RED +  "-----------------------\nbot disconnected or connection failed\n-----------------------" + Style.RESET_ALL)
+        print(Fore.RED + "-----------------------\nbot disconnected or connection failed\n-----------------------" + Style.RESET_ALL)
         logging.warning("bot disconnected")
 
     @Cog.listener()
     async def on_connect(self):
-        print(Fore.GREEN +  "-----------------------\nbot connected\n-----------------------" + Style.RESET_ALL)
+        print(Fore.GREEN + "-----------------------\nbot connected\n-----------------------" + Style.RESET_ALL)
         logging.info("bot connected")
