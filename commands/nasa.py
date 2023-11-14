0440d1ad705bdbb1d065acc5828810aa5418b72e
vlaner <vlaner@bk.ru>
2023-11-14 12:55:17 +0300

    картинка дня от NASA теперь работает

--- commands/nasa.py
+++ commands/nasa.py
@@ -1,28 +1,25 @@
-from discord import Embed
+from discord import Embed, Interaction, app_commands
 from discord.ext.commands import Cog, hybrid_command, Context
 from bot import Bot
 from .resources.exceptions import CustomCheckError
-name = "nasapict"
-description = "Картинка дня от NASA"
-
+from dependencies.api import nasa
 
 class Nasa(Cog):
-    def __init__(self, bot):
+    def __init__(self, bot, nasa_api: nasa.NasaApi):
         self.bot: Bot = bot
+        self.nasa_api = nasa_api
 
-    @hybrid_command(name=name, description=description)
-    async def nasapict(self, ctx: Context):
+    @app_commands.command(name="nasapict", description="Картинка дня от NASA")
+    async def nasapict(self, inter: Interaction):
+        resp = await self.nasa_api.get_image_of_the_day()
+
         embed = Embed(title="Картинка дня от NASA", color=0x0000FF)
-        try:    
-            resp = await self.nasa_api.get_response()
+        embed.set_image(url=resp['image'])
+        embed.set_footer(text=resp['title'])
 
-            embed.set_image(url=resp.image)
-            embed.set_footer(text=resp.title)
-
-            await ctx.send(embed=embed)
-        except Exception as e:
-            raise CustomCheckError(message="Не удалось получить картинку дня")
+        await inter.response.send_message(embed=embed)
 
 
 async def setup(bot):
-    await bot.add_cog(Nasa(bot))
+    nasa_api = nasa.NasaApi()
+    await bot.add_cog(Nasa(bot, nasa_api))
