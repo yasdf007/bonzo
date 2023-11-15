6658f5594f953e4f61d91a0df8cd69cfeabb41ed
vlaner <vlaner@bk.ru>
2023-11-15 21:30:24 +0300

    ролл теперь в коге Random

--- commands/Random.py
+++ commands/Random.py
@@ -7,6 +7,7 @@ from aiohttp import ClientSession
 
 from dependencies.api import youtube, dvach
 from config import YOUTUBE_API_KEY
+import random
 
 def get_random_prntsc_link():
     symbolsStr = "".join(random.choices(ascii_lowercase + digits, k=6))
@@ -32,36 +33,52 @@ async def get_random_imgur_link():
             i +=1
 
 
-class Random(GroupCog, group_name='random', group_description='Различные случайные данные'):
+class Random(Cog):
+    group = app_commands.Group(name="random", description="Различные случайные данные")
+
     def __init__(self, bot, youtube_sdk: youtube.YoutubeRandomApiSDK, random_tube: dvach.RandomtubeAPI):
         self.bot: Bot = bot
         self.random_tube = random_tube
         self.youtube_sdk = youtube_sdk
 
-    @app_commands.command(name='cat', description='Отправляет случайного кота')
+    @group.command(name='cat', description='Отправляет случайного кота')
     async def random_cat(self, inter: Interaction):
         await inter.response.send_message("https://cataas.com/cat")
     
-    @app_commands.command(name='prntsc', description='Отправляет случайное изображение из https://prnt.sc')
+    @group.command(name='prntsc', description='Отправляет случайное изображение из https://prnt.sc')
     async def random_prntsc(self, inter: Interaction):
         await inter.response.send_message(get_random_prntsc_link())
     
-    @app_commands.command(name='youtube', description='Отправляет случайное видео из https://youtube.com')
+    @group.command(name='youtube', description='Отправляет случайное видео из https://youtube.com')
     async def random_yt(self, inter: Interaction):
         link = await self.youtube_sdk.get_random_video()
         await inter.response.send_message(link) 
     
-    @app_commands.command(name='2ch', description='(18+) Отправляет случайное видео с https://2ch.hk')
+    @group.command(name='2ch', description='(18+) Отправляет случайное видео с https://2ch.hk')
     async def random_2ch(self, inter: Interaction):
         link = await self.random_tube.get_random_url()
         await inter.response.send_message(link) 
 
-    @app_commands.command(name='imgur', description='Отправляет случайное изображение из https://imgur.com')
+    @group.command(name='imgur', description='Отправляет случайное изображение из https://imgur.com')
     async def random_imgur(self, inter: Interaction):
         link = await get_random_imgur_link()
         if not link:
             return
         await inter.response.send_message(link)
+
+    @app_commands.command(name='roll', description='Ролл как в Dota 2')
+    async def dota_roll(self, inter: Interaction, number_from: int = 1, number_to: int = 100):
+        if number_to < 0:
+            number_to = 100
+
+        if number_from < 0:
+            number_from = 1
+
+        if number_from > number_to:
+            number_from, number_to = number_to, number_from
+            
+        random_num = random.randint(number_from, number_to)
+        await inter.response.send_message(f'Случайное число: {random_num}')
 
 async def setup(bot):
     yt_sdk = youtube.YoutubeRandomApiSDK(YOUTUBE_API_KEY)
